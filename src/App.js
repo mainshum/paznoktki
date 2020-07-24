430e7039a63820a0951e9443591ea847a91007e6
pasatoai <aleksander@aims.ai>
2020-07-24 23:34:53 +0200

    changed startpage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import nail from './nail.svg';
+import caption from './main_caption.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <div className="temp">
+        <img src={caption} alt="Paznoktki"></img>
         <img src={nail} className="nail" alt="nail"></img>
-        <h3>Paznoktki</h3>
       </div>
     </div>
   );
